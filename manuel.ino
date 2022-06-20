bfdcc1f4b6d2681a670af0aaceeceec8e22be3b9
muhammedemiraysan <89260017+muhammedemiraysan@users.noreply.github.com>
2022-06-20 22:54:10 +0300

    Update manuel.ino

--- manuel.ino
+++ manuel.ino
@@ -1,14 +1,19 @@
 #include <Servo.h>
-int pwmVal = 1470;
+int servo_hiz = 0;
+int servo1_hiz = 0;
+int servo2_hiz = 0;
+int servo3_hiz = 0;
+int servo4_hiz = 0;
+int servo5_hiz = 0;
+int servo_yon = 0;
+int servo1_yon = 1;
+int servo2_yon = 1;
+int servo3_yon = 1;
+int servo4_yon = 1;
+int servo5_yon = 1;
 int incomingByte = 0;
 byte servoPin = 3; byte servoPin1 = 5; byte servoPin2 = 6; byte servoPin3 = 9; byte servoPin4 = 10; byte servoPin5 = 11;
 Servo servo; Servo servo1; Servo servo2; Servo servo3; Servo servo4; Servo servo5;
-void Run(int pwmVal){servo.writeMicroseconds(pwmVal);}
-void Run1(int pwmVal){servo1.writeMicroseconds(pwmVal);}
-void Run2(int pwmVal){servo2.writeMicroseconds(pwmVal);}
-void Run3(int pwmVal){servo3.writeMicroseconds(pwmVal);}
-void Run4(int pwmVal){servo4.writeMicroseconds(pwmVal);}  
-void Run5(int pwmVal){servo5.writeMicroseconds(pwmVal);}
 
 void setup() {
 pinMode(13,OUTPUT);
@@ -32,13 +37,40 @@ Serial.println(incomingByte);
 
 }
 if (incomingByte == 49){
-  servo.writeMicroseconds(1600);
-  digitalWrite(13,HIGH);
-  }
+  servo_hiz = 170;
+  servo_yon = 1;
+  servo1_hiz = 170;
+  servo1_yon = 1;
+  servo2_hiz = 170;
+  servo2_yon = 1;
+  servo3_hiz = 170;
+  servo3_yon = 1;  
+  servo4_hiz = 170;
+  servo4_yon = 1;
+  servo5_hiz = 170;
+  servo5_yon = 1;
+  
+  digitalWrite(13,HIGH);}
 if (incomingByte == 50){
-  servo.writeMicroseconds(1470);
+  servo_hiz = 0;
+  servo_yon = 1;
+  servo1_hiz = 0;
+  servo1_yon = 1;
+  servo2_hiz = 0;
+  servo2_yon = 1;
+  servo3_hiz = 0;
+  servo3_yon = 1;  
+  servo4_hiz = 0;
+  servo4_yon = 1;
+  servo5_hiz = 0;
+  servo5_yon = 1;
   digitalWrite(13,LOW);
   }
-//Serial.print(" pwmVal: ");
-//Serial.println(pwmVal);
+servo.writeMicroseconds(1470  + (servo_hiz  * servo_yon));
+servo1.writeMicroseconds(1470 + (servo1_hiz  * servo1_yon));
+servo2.writeMicroseconds(1470 + (servo2_hiz  * servo2_yon));
+servo3.writeMicroseconds(1470 + (servo3_hiz  * servo3_yon));
+servo4.writeMicroseconds(1470 + (servo4_hiz  * servo4_yon));
+servo5.writeMicroseconds(1470 + (servo2_hiz  * servo2_yon));
+
 }
