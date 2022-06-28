21df218d9dcdd46288fd1e143a77d8b91de18004
muhammedemiraysan <89260017+muhammedemiraysan@users.noreply.github.com>
2022-06-29 01:33:46 +0300

    Update kumanda.ino

--- kumanda.ino
+++ kumanda.ino
@@ -1,85 +1,138 @@
+#include <Servo.h>
+int servo_hiz = 0;
+int servo1_hiz = 0;
+int servo2_hiz = 0;
+int servo3_hiz = 0;
+int servo4_hiz = 0;
+int servo5_hiz = 0;
+int servo6_hiz = 0;
+int servo6_yon = 1;
+int servo_yon = 0;
+int servo1_yon = 1;
+int servo2_yon = 1;
+int servo3_yon = 1;
+int servo4_yon = 1;
+int servo5_yon = 1;
+int servo7 = 100;
+int incomingByte = 0;
+byte servoPin = 6; byte servoPin1 = 3; byte servoPin2 = 5; byte servoPin3 = 10; byte servoPin4 = 9; byte servoPin5 = 7; byte servoPin6 = 11;
+Servo servo; Servo servo1; Servo servo2; Servo servo3; Servo servo4; Servo servo5; Servo servo6;
 
- 
-// Define Input Connections
-#define CH1 3
-#define CH2 5
-#define CH3 6
-#define CH4 9
-#define CH5 10
-#define CH6 11
-#define CH7 8
-// Integers to represent values from sticks and pots
-int ch1Value;
-int ch2Value;
-int ch3Value;
-int ch4Value; 
-int ch5Value;
-int ch6Value;
-int sagx = 0;  
-int sagy = 0;
-int solx = 0;  
-int soly = 0;
-int chh6;
- 
-// Read the number of a specified channel and convert to the range provided.
-// If the channel is off, return the default value
-int readChannel(int channelInput, int minLimit, int maxLimit, int defaultValue){
-  int ch = pulseIn(channelInput, HIGH, 30000);
-  if (ch < 100) return defaultValue;
-  return map(ch, 1000, 2000, minLimit, maxLimit);
+void setup() {
+pinMode(13,OUTPUT);
+Serial.begin(9600); 
+servo.attach(servoPin); servo.writeMicroseconds(1470);
+servo1.attach(servoPin1); servo1.writeMicroseconds(1470);
+servo2.attach(servoPin2); servo2.writeMicroseconds(1470);
+servo3.attach(servoPin3); servo3.writeMicroseconds(1470);
+servo4.attach(servoPin4); servo4.writeMicroseconds(1470);
+servo5.attach(servoPin5); servo5.writeMicroseconds(1470);
+servo6.attach(servoPin5); servo6.writeMicroseconds(1470);
+delay(7000);
 }
- 
-// Read the switch channel and return a boolean value
-bool readSwitch(byte channelInput, bool defaultValue){
-  int intDefaultValue = (defaultValue)? 100: 0;
-  int ch = readChannel(channelInput, 0, 100, intDefaultValue);
-  return (ch > 50);
-}
- 
-void setup(){
-  // Set up serial monitor
-  Serial.begin(9600);
-  
-  // Set all pins as inputs
-  pinMode(CH1, INPUT);
-  pinMode(CH2, INPUT);
-  pinMode(CH3, INPUT);
-  pinMode(CH4, INPUT);
-  pinMode(CH5, INPUT);
-  pinMode(CH6, INPUT);
-  pinMode(CH7, INPUT);
-}
- 
- 
 void loop() {
-  
-  // Get values for each channel
-  ch1Value = readChannel(CH1, -100,100, 0);
-  ch2Value = readChannel(CH2, -100,100, 0);
-  ch3Value = readChannel(CH3, -100,100, -100);
-  ch4Value = readChannel(CH4, -100,100, 0);
-  ch5Value = readChannel(CH5, -512,512, 0);
-  ch6Value = readChannel(CH6,-512,512,0);
-  sagx = map(ch4Value,-100,100,0,1024);
-  sagy = map(ch2Value,-100,100,0,1024);
-  soly = map(ch3Value,-100,100,0,1024);
-  solx = map(ch1Value,-100,100,0,1024);
+if (Serial.available() > 0) {
 
-if(sagx > 470 && sagx < 530){sagx = 500;}
-if(sagy > 470 && sagy < 530){sagy = 500;}
-  if(solx > 470 && solx < 530){solx = 500;}if(soly > 470 && soly < 530){soly = 500;}
-  sagx = (sagx-500) * -1; sagy = (sagy-500) * 1; solx = (solx-500) * 1; soly = (soly-500) * 1;
-  Serial.print(String(sagx));      
-  Serial.print("n");
-  Serial.print(String(sagy));          
-  Serial.print("n");
-  Serial.print(String(solx));      
-  Serial.print("n");
-  Serial.print(String(soly));      
-  Serial.print("n");
-  Serial.print(String(ch6Value));
-  Serial.print("n");
-  Serial.print(String(ch5Value));
-  Serial.println("n");
-  //delay(100);
+incomingByte = Serial.read(); // read the incoming byte:
+
+Serial.print(" I received:");
+
+Serial.println(incomingByte);
+
+}
+if (incomingByte == 49){
+  servo_hiz = 250;
+  servo_yon = 1;
+  servo2_hiz = 275;
+  servo2_yon = -1;
+  servo3_hiz = 150;
+  servo3_yon = -1;  
+  servo5_hiz = 275;
+  servo5_yon = 1;
+  
+  digitalWrite(13,HIGH);}
+if (incomingByte == 50){
+  servo_hiz = 0;
+  servo_yon = 1;
+  servo1_hiz = 0;
+  servo1_yon = 1;
+  servo2_hiz = 0;
+  servo2_yon = 1;
+  servo3_hiz = 0;
+  servo3_yon = 1;  
+  servo4_hiz = 0;
+  servo4_yon = 1;
+  servo5_hiz = 0;
+  servo5_yon = -1;
+  digitalWrite(13,LOW);
+  }
+if (incomingByte == 51){
+  servo_hiz = 100;
+  servo_yon = -1;
+  servo2_hiz = 100;
+  servo2_yon = 1;
+  servo3_hiz = 100;
+  servo3_yon = -1;  
+  servo5_hiz = 100;
+  servo5_yon = 1;
+  digitalWrite(13,LOW);
+  }
+if (incomingByte == 52){
+  servo_hiz = 100;
+  servo_yon = 1;
+  servo2_hiz = 100;
+  servo2_yon = -1;
+  servo3_hiz = 100;
+  servo3_yon = 1;  
+  servo5_hiz = 100;
+  servo5_yon = -1;
+  }
+if (incomingByte == 53){
+  servo1_hiz = 400;
+  servo1_yon = -1;
+  servo4_hiz = 400;
+  servo4_yon = 1;
+  }
+if (incomingByte == 54){
+  servo1_hiz = 400;
+  servo1_yon = 1; 
+  servo4_hiz = 400;
+  servo4_yon = -1;
+  }
+if (incomingByte == 55){
+  servo_hiz = 250;
+  servo_yon = -1;
+  servo2_hiz = 275;
+  servo2_yon = 1;
+  servo3_hiz = 150;
+  servo3_yon = 1;  
+  servo5_hiz = 275;
+  servo5_yon = -1;
+  }
+if(incomingByte == 56){
+  servo6_hiz = 300;
+  servo6_yon = 1;
+  }
+if(incomingByte == 57){
+  servo6_hiz = 300;
+  servo6_yon = 1;
+  }
+if(incomingByte == 58){
+  if(servo7 > 0){
+    servo7 = servo7-10;
+    }
+  }
+if(incomingByte == 59){
+  if(servo7 < 100){
+    servo7 = servo7+10;
+    }
+  }
+servo.writeMicroseconds(1470  + (servo_hiz  * servo_yon));
+servo1.writeMicroseconds(1470 + (servo1_hiz  * servo1_yon));
+servo2.writeMicroseconds(1470 + (servo2_hiz  * servo2_yon));
+servo3.writeMicroseconds(1470 + (servo3_hiz  * servo3_yon));
+servo4.writeMicroseconds(1470 + (servo4_hiz  * servo4_yon));
+servo5.writeMicroseconds(1470 + (servo5_hiz  * servo5_yon));
+servo6.writeMicroseconds(1470 + ((servo6_hiz * servo7) * servo6_yon));
+
 }
